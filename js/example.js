feaf5d70155ec7c491dacd0bdffbb3a6a4b1338a
Oleksandra9 <sashakrytska@gmail.com>
2021-11-24 16:35:21 +0000

    +

---
+++ js/example.js
@@ -0,0 +1,21 @@
+
+// let elementHeight = 1300.16472
+// console.log(Number(elementHeight.toFixed(2)));
+
+// ---------------------------------------------
+
+// const a = 2;
+// const b = 5;
+
+// console.log(Math.pow(a,b)); 
+// console.log(a ** b);
+
+// ---------------------------------------
+
+let base = prompt("Введи любое произвольное число от 1 до 9");
+base = Number(base);
+
+let power = prompt("введи любое произвольное число для степени число от 1 до 9");
+power = Number(power);
+
+console.log(base ** power);
